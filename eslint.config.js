12411b9399b0098e951d6b952154b9b3bf49d24f
GildedPleb <gildedpleb@protonmail.com>
2024-03-20 07:59:12 -0700

    Clean config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,10 @@
 import currentThing from "./src/config.js";
+// import pathmark from "eslint-plugin-pathmark"
 
-export default [...currentThing()];
+export default [
+  ...currentThing(),
+  // pathmark.configs.flat,
+];
 
 // export default [...currentThing({ disable: ["eslint-config-airbnb-base"] })];
 
